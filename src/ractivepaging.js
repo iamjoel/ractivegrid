30ee45ad8d1a9796ba7af21a71fc3252950bbbf6
joel <iamjoel007@gmail.com>
2014-06-19 16:41:56 +0800

    add skin

--- src/ractivepaging.js
+++ src/ractivepaging.js
@@ -5,12 +5,18 @@
 	};
 
 	var template =
-		'<ul>'
+		'<ul class="ra-paging">'
+			+ '<li class="{{getPrevStatus(pageAt)}}">'
+				+ '<a on-click="prevPage" href="javascript:void(0);">上一页</a>'
+			+'</li>'
 		+ '{{#pageArray:i}}'
-			+ '<li class="{{getStatus(.,pageAt)}}">'
+			+ '<li class="{{getStatus(., pageAt)}}">'
 			+ '<a on-click="changePage" href="javascript:void(0);" data-index="{{.}}">{{.}}</a>'
 			+ '</li>'
 		+ '{{/pageArray}}'
+			+ '<li class="{{getNextStatus(pageAt, pageArray.length)}}">'
+				+ '<a on-click="nextPage" href="javascript:void(0);">下一页</a>'
+			+'</li>'
 		+ '</ul>';
 	function Paging(param){
 		var self = this;
@@ -31,18 +37,42 @@
             	pageAt: param.pageAt,
             	pageArray: makePageArray(param.pageNum),
             	getStatus: function(index, pageAt){
-            		return index === pageAt ? 'page-inactive' : 'page-active';
+            		return index === pageAt ? 'active' : '';
+            	},
+            	getPrevStatus: function(pageAt){
+            		return pageAt <= 1 ? 'disabled' : '';
+            	},
+            	getNextStatus: function(pageAt, pageNum){
+            		return pageAt >= pageNum ? 'disabled' : '';
             	}
             }
         });
 
         this.paging.on('changePage', function(event){
         	var pageAt = parseInt($(event.node).text(), 10);
-        	this.set('pageAt', pageAt);
+        	changPage(pageAt);
+        });
+
+        this.paging.on('prevPage', function(event){
+        	var pageAt = this.get('pageAt') - 1;
+        	if(pageAt >= 1){
+        		changPage(pageAt);
+        	}
+        });
+
+        this.paging.on('nextPage', function(event){
+        	var pageAt = this.get('pageAt') + 1;
+        	if(pageAt <= param.pageNum){
+        		changPage(pageAt);
+        	}
+        });
+
+        function changPage(pageAt){
+        	self.paging.set('pageAt', pageAt);
         	if(_.isFunction(param.pageChange)){
         		param.pageChange(pageAt);
         	}
-        });
+        }
 
 	}
 
