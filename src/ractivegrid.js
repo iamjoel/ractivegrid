bc00fa51a0f9bd96ce4280a48d4d814c9f2fd576
iamjoel <iamjoel007@gmail.com>
2014-07-13 22:55:49 +0800

    Updates

--- src/ractivegrid.js
+++ src/ractivegrid.js
@@ -12,7 +12,8 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
     var undef = undefined;
     var defaultPageParam = {
         pageLimit: 10,
-        pageSizeParamName: 'pageSize'
+        pageSizeParamName: 'pageSize',
+        enableSelectRow: false
     }
     var Ractivegrid = function(param) {
         var validMsg = validParam(param);
@@ -32,13 +33,14 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
             paging = param.paging;
             param.pageParam = $.extend({}, defaultPageParam, param.pageParam);
             endIndex = param.pageParam.pageLimit;
-            if(!isAysn){
+            if (!isAysn) {
                 var allDataLen = this.getRendData(param.data, 0, Infinity, param.format).length;
-                param.paging.setPageNum(self.calPageNum(allDataLen, param.pageParam.pageLimit));// 异步的是给外部来算的
+                param.paging.setPageNum(self.calPageNum(allDataLen, param.pageParam.pageLimit)); // 异步的是给外部来算的
             }
         }
 
         var renderData = isAysn ? [] : this.getRendData(param.data, startIndex, endIndex, param.format);
+
 
         this.grid = new Ractive({
             el: param.el,
@@ -46,6 +48,7 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
             data: {
                 data: renderData,
                 columns: param.columns,
+                enableSelectRow: param.enableSelectRow,
                 getCellContent: function(data, columns, rowIndex, colIndex, fromRaw, toRaw) {
                     var rowData = data[rowIndex];
                     var cellData = rowData;
@@ -80,8 +83,9 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
                 return;
             }
             var $td = $(event.node).closest('td');
-            var rowIndex = $td.data('row-index');
-            var colIndex = $td.data('col-index');
+            var cellInfo = getCellInfo(event, this);
+            var rowIndex = cellInfo.rowIndex;
+            var colIndex = cellInfo.colIndex;
             this.set('data.' + rowIndex + '.' + colIndex + '.isEdit', true);
             this.set('editing', [rowIndex, colIndex]);
             $td.find(':text').focus();
@@ -89,10 +93,10 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
 
         this.grid.on('blur', function(event) {
             var $input = $(event.node);
-            var $td = $input.closest('td');
-            var rowIndex = $td.data('row-index');
-            var colIndex = $td.data('col-index');
-            var columnName = this.get(event.keypath + '.name');
+            var cellInfo = getCellInfo(event, this);
+            var rowIndex = cellInfo.rowIndex;
+            var colIndex = cellInfo.colIndex;
+            var columnName = cellInfo.columnName;
             var inputData = $input.val();
             var toRaw = this.get(event.keypath + '.toRaw');
             this.set('data.' + rowIndex + '.' + colIndex + '.isEdit', false);
@@ -106,6 +110,8 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
             }
         });
 
+
+
         // 修复ie，Firefox光标在输入框前面的bug
         this.grid.observe('editing', function(newValue, oldValue, keypath) {
             var rowIndex = newValue[0];
@@ -118,13 +124,24 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
             if (input.setSelectionRange) { //ie9+,firefox,chrome
                 input.setSelectionRange(endPlace, endPlace);
             } else {
-                // setTimeout(function(){
                 var range = input.createTextRange();
                 range.moveEnd("character", endPlace);
                 range.moveStart("character", endPlace);
-                // }, 100);
             }
         });
+
+        // 全选按钮
+        this.$table = null;
+        this.grid.on('all-checked', function(event) {
+            var $checkbox = $(event.node);
+            if (!self.$table) {
+                self.$table = $checkbox.closest('table');
+            }
+            var $checkboxs = self.$table.find('tbody tr td:first-child :checkbox');
+            $checkboxs.prop('checked', $checkbox.prop('checked'));
+        });
+
+        this.bindCellEvent();
 
 
         if (paging) {
@@ -155,6 +172,83 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
 
     };
 
+
+
+    /*
+     * 给单元格绑用户自定义事件
+     */
+    Ractivegrid.prototype.bindCellEvent = function() {
+        var self = this;
+        // 暂时只支持click
+        this.grid.on('click', function(event) {
+            self.clickHandler(self, event);
+        });
+    };
+
+    Ractivegrid.prototype.clickHandler = function(ctx, event) {
+        var parma = ctx.param;
+        var cellInfo = getCellInfo(event, this.grid);
+        var columnName = cellInfo.columnName;
+        var rowIndex = cellInfo.rowIndex;
+        var colIndex = cellInfo.colIndex;
+        var eventInfo = getEventInfo(parma, colIndex, 'click');
+        var rowdata = ctx.grid.get('data.' + rowIndex);
+        var cellData = ctx.grid.get('data.' + rowIndex + '.' + columnName);
+        var target = event.original.currentTarget;
+        var $parent = $(event.node);
+        if (eventInfo) {
+            eventInfo.forEach(function(each) {
+                var canExcute = true;
+                if (each.el) { // 判断点击的el，是否为目标el todo 有问题
+                    if (!$parent.find(each.el) || !$parent.find(each.el)[0] === target) {
+                        canExcute = false;
+                    }
+                }
+                if (canExcute) {
+                    each.fn(cellData, rowdata);
+                }
+            });
+        }
+    };
+
+    Ractivegrid.prototype.getSelectRowsInfo = function(){
+        var self = this;
+        if(!this.$table){
+            this.$table = $(this.param.el).find('table');
+        }
+        var $SelectChecked = this.$table.find('tbody tr td:first-child :checkbox:checked');
+        var info = [];
+        $SelectChecked.each(function(){
+            var $checkbox = $(this);
+            var rowIndex = $checkbox.data('row-index');
+            info.push(self.grid.get('data.' + rowIndex));
+
+        });
+        return info;
+    }
+
+
+    function getEventInfo(param, colIndex, type) {
+        var columns = param.columns;
+        var cellParm = columns[colIndex];
+        var events = cellParm.event;
+        if (!events) {
+            return false;
+        }
+        var eventInfo = []; // {'click':..,'click btn':...}
+        var eventsReg = /^(click|hover)(?: (.*))?$/;
+        for (var key in events) {
+            var regRes = eventsReg.exec(key);
+            if (regRes && regRes[1] === type) {
+                eventInfo.push({
+                    el: regRes[2],
+                    fn: events[key]
+                });
+            }
+        }
+        return eventInfo;
+    }
+
     /*
      * 包含 startIndx，不包含endIndex
      */
@@ -184,7 +278,8 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
         }
 
         $.ajax({
-            url: url
+            url: url,
+            dataType: 'json'
         }).done(function(rawData) {
             var data = self.getRendData(rawData, 0, Infinity, param.format);
             if (!$.isArray(data)) {
@@ -223,6 +318,16 @@ define(['text!ractivegrid-template', 'css!ractivegrid-css'], function(template) 
         }
         return pageSize;
     };
+
+    function getCellInfo(event, grid) {
+        var info = {};
+        var $input = $(event.node);
+        var $td = $input.closest('td');
+        info.rowIndex = $td.data('row-index');
+        info.colIndex = $td.data('col-index');
+        info.columnName = grid.get(event.keypath + '.name');
+        return info;
+    }
 
     function validParam(param) {
         if (typeof param !== 'object') {
